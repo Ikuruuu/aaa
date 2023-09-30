3b7a56728189853e28551bbdb34b452f98b6d531
Ikuruuu <s21a3075vp@s.chibakoudai.jp>
2023-09-30 16:40:30 +0900

    9-2終了

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,10 +15,19 @@
                 <input type="text" name=post[title] placeholder="タイトル">
             </div>
             <div class="body">
+            <div class="category">
+                <h2>Category</h2>
+                <select name="post[category_id]">
+                    @foreach($categories as $category)
+                         <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                 </select>
+            </div>
                 <h2>Body</h2>
                 <textarea name="post[body]" placeholder="今日も一日お疲れさまでした。"></textarea>
             </div>
             <input type="submit" value="store"/>
+            
         </form>
         <div class="footer">
             <a href="/">戻る</a>
