e64421f8b8b7d16d5e52d28ab711107237369cd3
Ikuruuu <s21a3075vp@s.chibakoudai.jp>
2023-10-05 18:22:51 +0900

    9-3終了

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
+    <x-app-layout>
+    <x-slot name="header">
+    Index
+    </x-slot>
     <body>
         <h1>Blog Name</h1>
         <a href="/posts/create">create</a>
@@ -37,5 +35,7 @@
         }
     }
 </script>
+     {{ Auth::user()->name }}
     </body>
+    </x-app-layout>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <x-app-layout>
+    <x-slot name="header">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -6,6 +8,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
+    </x-slot>
     <body>
         <h1 class="title">
             {{ $post->title }}
@@ -25,4 +28,5 @@
             <a href="/">戻る</a>
         </div>
     </body>
+    </x-app-layout>
 </html>
